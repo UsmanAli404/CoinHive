b5fea1ca5a7dcdeb5db09b00c4a310b995f8068b
hassan <hassanyousaf941@gmail.com>
2025-04-23 19:55:15 +0500

    -added posts requests (get coins, get activites, get market data)

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,45 @@ import 'dotenv/config';
 import cookieParser from "cookie-parser"
 import authenticationRouter from "./routes/routes.js"
 import userRouter from "./routes/userroutes.js";
-const app = express();
-const port  =  process.env.PORT || 4000
-connectDB();
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({Credentials:true}))
+import marketRoutes from "./routes/marketRoutes.js";
+import { streamMarketData } from "./controllers/marketController.js";
 
-app.get('/',(req,res)=>res.send("Server is Working"))
-app.use('/api/auth',authenticationRouter)
-app.use('/api/user',userRouter)
-app.listen(port,()=>console.log(`Sever Started on port:${port}`))
+// WebSocket setup (importing default export from 'ws')
+import WebSocket from 'ws';
+
+const app = express();
+const port  = process.env.PORT || 4000;
+
+connectDB();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ Credentials: true }));
+
+app.get('/', (req, res) => res.send("Server is Working"));
+app.use('/api/auth', authenticationRouter);
+app.use('/api/user', userRouter);
+app.use('/api/market', marketRoutes);
+
+
+const server = app.listen(port, () => console.log(`Server started on port: ${port}`));
+
+const wss = new WebSocket.Server({ server });
+
+//websockets
+wss.on('connection', (ws) => {
+    console.log('New WebSocket connection');
+    streamMarketData(ws);
+  
+    ws.on('message', (message) => {
+      console.log('Received message:', message);
+    });
+  
+    ws.on('error', (error) => {
+      console.error('WebSocket error:', error);
+    });
+  
+    ws.on('close', () => {
+      console.log('WebSocket connection closed');
+    });
+});
+
